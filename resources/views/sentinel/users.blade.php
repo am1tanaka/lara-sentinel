81328c241999eede06760621b52fa5a786c1eb94
YuTanaka <YRK00337@nifty.com>
2016-07-02 01:50:09 +0900

    ユーザー一覧用の実装

--- resources/views/sentinel/users.blade.php
+++ resources/views/sentinel/users.blade.php
@@ -5,10 +5,11 @@
     <div class="row">
         <div class="col-md-12">
 
+            @if ($role=="admin")
+
             <form class="col" role="form" method="POST" action="{{url('users')}}">
                 {{ csrf_field() }}
 
-                @if ($role=="admin")
                 <div class="panel panel-default">
                     <div class="panel-heading">ユーザー新規登録</div>
                     <div class="panel-body">
@@ -32,33 +33,107 @@
 
                     </div>
                 </div>
-                @endif
-
-                <div class="panel panel-default">
-                    <div class="panel-heading">ユーザー一覧</div>
-
-                    <table class="table table-striped">
-
-                        <tbody>
-                            @foreach($users as $user)
-                                <tr>
-                                    <td>
-                                        {{$user->first_name}}
-                                    </td>
-                                    <td>
-                                        @include('parts.role-select', ['user' => $user, 'roles' => $roles])
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                </table>
-
-                    <div class="panel-body">
-
-                    </div>
-                </div>
-
             </form>
+
+            @endif
+
+            <table class="table table-striped table-bordered table-hover">
+                <thead>
+                    <tr>
+                        <th>
+                            ユーザー名
+                        </th>
+                        <th>
+                            メールアドレス
+                        </th>
+                        <th>
+                            ロール
+                        </th>
+                        <th>
+                            操作
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($users as $user)
+                        <tr>
+                            <td>
+                                <input type="text" name="user_{{$user->id}}_name" value="{{$user->first_name}}" maxlength='255' size='20'>
+                            </td>
+                            <td>
+                                <input type="text" name="user_{{$user->id}}_email" value="{{$user->email}}" maxlength='255' size='30'>
+                            </td>
+                            <td>
+                                @include('parts.role-select', ['user' => $user, 'roles' => $roles])
+                            </td>
+                            <td>
+                                <!-- Button trigger modal -->
+                                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalUpdate-{{$user->id}}">
+                                    <i class="fa fa-btn fa-refresh"></i>変更実行
+                                </button>
+                                <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#modalDelete-{{$user->id}}">
+                                    <i class="fa fa-btn fa-trash"></i>削除
+                                </button>
+
+                                <!-- 更新モーダル-->
+                                <div class="modal fade" id="modalUpdate-{{$user->id}}" tabindex="-1" role="dialog">
+                                  <div class="modal-dialog">
+                                    <div class="modal-content">
+                                      <div class="modal-header">
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                        <h4 class="modal-title">ユーザー情報を変更しますか？</h4>
+                                      </div>
+                                      <div class="modal-footer">
+                                          <form action="{{ url('users/'.$user->id) }}" method="POST">
+                                              {{ csrf_field() }}
+                                              {{ method_field('PUT') }}
+
+                                              <button type="submit" id="update-{{ $user->id }}" class="btn btn-primary">
+                                                  <i class="fa fa-btn fa-check"></i>  はい
+                                              </button>
+                                              <button type="button" class="btn btn-default" data-dismiss="modal">
+                                                  <i class="fa fa-btn fa-close"></i>いいえ</button>
+                                          </form>
+
+                                      </div>
+                                    </div><!-- /.modal-content -->
+                                  </div><!-- /.modal-dialog -->
+                                </div><!-- /.modal -->
+
+                                <!-- 削除モーダル-->
+                                <div class="modal fade" id="modalDelete-{{$user->id}}" tabindex="-1" role="dialog">
+                                  <div class="modal-dialog">
+                                    <div class="modal-content">
+                                      <div class="modal-header">
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                        <h4 class="modal-title">指定のユーザーを削除しますか？</h4>
+                                      </div>
+                                      <div class="modal-body">
+                                          {{$user->first_name}} ： {{$user->email}}
+                                      </div>
+                                      <div class="modal-footer">
+                                          <form action="{{ url('users/'.$user->id) }}" method="POST">
+                                              {{ csrf_field() }}
+                                              {{ method_field('DELETE') }}
+
+                                              <button type="submit" id="update-{{ $user->id }}" class="btn btn-primary">
+                                                  <i class="fa fa-btn fa-check"></i>  はい
+                                              </button>
+                                              <button type="button" class="btn btn-default" data-dismiss="modal">
+                                                  <i class="fa fa-btn fa-close"></i>いいえ</button>
+                                          </form>
+
+                                      </div>
+                                    </div><!-- /.modal-content -->
+                                  </div><!-- /.modal-dialog -->
+                                </div><!-- /.modal -->
+
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+
         </div>
     </div>
 </div>
