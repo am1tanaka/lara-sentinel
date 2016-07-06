2b3085c849e8bdc0d9436f4e93bfe363196297da
YuTanaka <YRK00337@nifty.com>
2016-07-06 13:31:32 +0900

    ユーザー管理画面のテスト用ルートを削除

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,17 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// TODO: 動作確認が終わったら削除する
-Route::get('test/{role}/{routename}', function($role, $routename){
-    return view('sentinel.users',
-    [
-        'role' => $role,
-        'method' => $routename,
-        'roles' => Sentinel::getRoleRepository()->all(),
-        'users' => Sentinel::getUserRepository()->all()
-    ]);
-});
-
 Route::get('activate/{email}/{code}', 'Sentinel\SentinelController@activate');
 
 Route::get('login', function() {return view('auth.login');});
